d7b3c7c54532e1074be65b1ccba3b1981e85438a
rikinhim <rikinhim@gmail.com>
2022-04-11 14:15:35 -0400

    replace credentials with variables in .env file

--- server/db/db.js
+++ server/db/db.js
@@ -1,6 +1,6 @@
 const Sequelize = require("sequelize");
 
-const db = new Sequelize(process.env.DATABASE_URL || "postgres://messenger:1234@localhost:5432/messenger", {
+const db = new Sequelize(process.env.DATABASE_URL || "postgres://DATABASE_USER:DATABASE_PASSWORD@localhost:5432/messenger", {
   logging: false
 });
 
