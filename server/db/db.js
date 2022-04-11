4483bf14b4d58f34c386925dad469f37e0e0e1f7
rikinhim <rikinhim@gmail.com>
2022-04-11 19:55:53 -0400

    remove the hardcode database url, added to local .env file

--- server/db/db.js
+++ server/db/db.js
@@ -1,6 +1,7 @@
 const Sequelize = require("sequelize");
+require("dotenv").config();
 
-const db = new Sequelize(process.env.DATABASE_URL || `postgres://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@localhost:5432/messenger`, {
+const db = new Sequelize(process.env.DATABASE_URL, {
   logging: false
 });
 
