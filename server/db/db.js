1c44621bd6c12a0a67a28c04df6a0e4efe2e064a
rikinhim <rikinhim@gmail.com>
2022-04-11 14:55:08 -0400

    fix .env varaibles

--- server/db/db.js
+++ server/db/db.js
@@ -1,6 +1,6 @@
 const Sequelize = require("sequelize");
 
-const db = new Sequelize(process.env.DATABASE_URL || "postgres://DATABASE_USER:DATABASE_PASSWORD@localhost:5432/messenger", {
+const db = new Sequelize(process.env.DATABASE_URL || `postgres://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@localhost:5432/messenger`, {
   logging: false
 });
 
