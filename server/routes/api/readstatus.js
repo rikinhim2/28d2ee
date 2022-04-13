72c935cd920c6ad669a809a399436fddce160f1a
rikinhim <rikinhim@gmail.com>
2022-04-13 13:43:57 -0400

    added api for readstatus

---
+++ server/routes/api/readstatus.js
@@ -0,0 +1,27 @@
+const router = require("express").Router();
+const { ReadStatus } = require("../../db/models");
+
+// to update the read status, given converstaion id
+router.put("/", async (req, res, next) => {
+  try {
+    const convoId = req.body.conversationId;
+    const userId = req.user.id;
+
+    // set the messageRead to true for the same conversation
+    const [update] = await ReadStatus.update(
+      { messageRead: true },
+      {
+        where: {
+          conversationId: convoId,
+          receiverId: userId,
+          messageRead: false,
+        }
+      }
+    );
+    res.json({ update })
+  } catch (error) {
+    next();
+  }
+});
+
+module.exports = router;
